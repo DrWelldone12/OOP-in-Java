92dbdf151c7e16620af0ed42ffc4fd82ea6835ae
DrWelldone12 <144967626+DrWelldone12@users.noreply.github.com>
2024-03-02 03:25:18 +0000

    Update Triangle.java

--- Triangle.java
+++ Triangle.java
@@ -2,7 +2,7 @@ package com.company;
 import java.util.Scanner;
 public class Triangle {
     public static void main(String[] args) {
-        /*System.out.println("      T      ");
+        /*System.out.println("    T      ");
         System.out.println("     TTT     ");
         System.out.println("    TTTTT    ");
         System.out.println("   TTTTTTT   ");
